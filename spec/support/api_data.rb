1155c63bf02ce4b9d81e5fab0f71438c9a640cc1
henrique montalione <henrique.montalione@gmail.com>
2018-07-02 20:18:57 -0300

    setting api mock

--- spec/support/api_data.rb
+++ spec/support/api_data.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
   config.before(:each) do
     
-    stub_request(:get, /currencydatafeed.com/ )
+    stub_request(:get, /min-api.cryptocompare.com/ )
     .with(headers: {
       'Accept'=>'*/*'
     }).to_return(status: 200, body: '
