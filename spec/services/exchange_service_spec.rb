bbe45abb7834fdbebaebeaa51e70d9e48c552c24
henriqueMontalione <henrique.montalione@gmail.com>
2018-07-04 12:17:51 -0300

    removing debugguer

--- spec/services/exchange_service_spec.rb
+++ spec/services/exchange_service_spec.rb
@@ -6,7 +6,7 @@ describe 'Currency' do
   it 'exchange' do
     amount = rand(0..9999)
     res = ExchangeService.new("USD", "BRL", amount).perform2()
-    binding.pry
+    #binding.pry
     expect(res.is_a? Numeric).to eql(true)
     expect(res != 0 || amount == 0).to eql(true)
   end
